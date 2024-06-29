a698b9596348d9d5faf11e0eb2264d20ca88b7f8
ricardemese <ricardm1995@gmail.com>
2024-06-29 19:18:44 +0000

    First Commit

--- src/app.js
+++ src/app.js
@@ -5,7 +5,37 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
+let who = ["The dog", "My grandma", "The mailman", "My bird", "My neighboord"];
+let action = ["ate", "peed", "crushed", "broke", "smoke"];
+let what = ["my homework", "my phone", "the car", "my computer", "my lunch"];
+let when = [
+  "before the class",
+  "when I was sleeping",
+  "while I was exercising",
+  "during my lunch",
+  "while I was praying"
+];
+
+function getRandomNumber(a, b) {
+  return Math.floor(Math.random() * (b - a + 1)) + a;
+}
+
+function getRandom(anyArray) {
+  let max = anyArray.length - 1;
+  let min = 0;
+  let random = getRandomNumber(min, max);
+  return anyArray[random];
+}
+
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  console.log("Who reads it, is silly!");
+  document.querySelector("#excuse").innerHTML =
+    getRandom(who) +
+    " " +
+    getRandom(action) +
+    " " +
+    getRandom(what) +
+    " " +
+    getRandom(when);
 };
